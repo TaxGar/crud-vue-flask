a0fae386638b791eb8d414298b6d49033166be69
Jhon Garcia <124848563+TaxGar@users.noreply.github.com>
2025-06-19 16:23:19 -0500

    Update server.py

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ app = Flask(__name__,
             static_folder='./crud-vuejs/dist/assets',
             template_folder='./crud-vuejs/dist')
 
-cors = CORS(app, resources={r"/*": {"origins": ['https://crud-vue-flask-jhon.web.app']}})
+cors = CORS(app, resources={r"/*": {"origins": ['https://crud-vue-flask-jhon.web.app', 'http://localhost:5173']}})
 
 
 API_KEY = "clave_super_secreta_123"  # cámbiala por una más segura
